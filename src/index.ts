0a9a55df57f7e83c970cc6bd1a261718b4f538ea
czM1K3 <misamadera1@gmail.com>
2023-01-17 11:44:27 +0100

    First commit

---
+++ src/index.ts
@@ -0,0 +1,49 @@
+import { serve } from "std/http/server.ts";
+import { Server } from "socketio";
+import { z } from "zod";
+
+const io = new Server({
+	cors: {
+		origin: "*",
+	},
+});
+
+type outgoingMessageType = {
+	sourceId: string;
+	message: string;
+	type: string;
+};
+
+const incommingMessageSchema = z.object({
+	targetId: z.string(),
+	message: z.string(),
+	type: z.string(),
+});
+
+io.on("connection", (socket) => {
+	socket.emit("id", socket.id);
+
+	socket.on("message", (message: string) => {
+		try {
+			const parsedMessage = JSON.parse(message);
+			const data = incommingMessageSchema.parse(parsedMessage);
+			
+			const out: outgoingMessageType = {
+				message: data.message,
+				sourceId: socket.id,
+				type: data.type,
+			};
+
+			const target = io.of("/").sockets.get(data.targetId);
+			if (!target)
+				throw new Error("Client not found");
+			target.emit("message", JSON.stringify(out));
+		} catch (_e) {
+			// console.log(e.message);
+		}
+	});
+});
+
+await serve(io.handler(), {
+	port: 3000,
+});
